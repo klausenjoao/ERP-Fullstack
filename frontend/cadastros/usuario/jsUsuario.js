deaeccb4e5a1c3bdc8b22f14ffe32201d9bf6c5c
João vitor Klausen <joaovitor_klausen@hotmail.com>
2024-11-19 22:50:52 -0300

    Ajustes para criação do usuário

---
+++ frontend/cadastros/usuario/jsUsuario.js
@@ -0,0 +1,6 @@
+const openModal = () =>
+    document.getElementById("modal-cadastrar").classList.add("active");
+  
+  const closeModal = () => {
+    document.getElementById("modal-cadastrar").classList.remove("active");
+  };
