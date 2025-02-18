2542b73d2452969aa82e576f12b225bd1c04f951
João Vitor Dos Santos Klausen <129987435+klausenjoao@users.noreply.github.com>
2025-02-18 19:10:24 -0300

    Comentarios usuarios

--- backend/src/models/usuarios.js
+++ backend/src/models/usuarios.js
@@ -1,15 +1,18 @@
 const connection = require('./connection')
 
+//Traz todos usuarios cadastrados
 const getAllUsuarios = async () =>{
     const [usuarios] = await connection.execute('SELECT*FROM usuarios')
     return usuarios;
 }
 
+//Traz a quantidade total de usarios
 const getTotal = async () =>{
     const [totalUsuarios] = await connection.execute (`SELECT COUNT(*) AS total FROM usuarios;`)
     return totalUsuarios;
   }
 
+//Cria o usuario no banco
 const createUsuarios = async (usuarios) =>{
     const {usu_nome, usu_login, usu_senha, usu_ativo} = usuarios;
     const dateFormatted = new Date().toISOString().slice(0, 19).replace('T', ' ');
@@ -21,16 +24,19 @@ const createUsuarios = async (usuarios) =>{
     return {insertId:createUsuarios.insertId};
 }
 
+//Traz o usuario baseado no id
 const getlUsuarioEspecifico = async (usu_id) =>{
     const [getUsuario] = await connection.execute('SELECT*FROM usuarios where usu_id=?',[usu_id])
     return getUsuario
 }
 
+//deleta o usuario baseado no id
 const deleteUsuarios = async (usu_id) =>{
     const removedUsuarios = await connection.execute('DELETE from usuarios where usu_id=?',[usu_id])
     return removedUsuarios
 }
 
+//Atualiza o usuario, editar
 const updateUsuario = async(usu_id, usuarios)=>{
     const {usu_nome, usu_login, usu_senha, usu_ativo}= usuarios;
 
